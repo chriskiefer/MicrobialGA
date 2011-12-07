2ed033dd483d29e6e71ba558973e292761ba1390
Chris Kiefer <chrisk13fer@gmail.com>
2011-12-07 09:22:29 +0000

    Initial Commit

---
+++ MicrobialGA/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MicrobialGA
+//
+//  Created by Chris Kiefer on 07/12/2011.
+//  Copyright (c) 2011 Goldsmiths Creative Computing. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
